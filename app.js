ba2dda0ddc5d1ecba434d1c5cda29aa525f276f3
AitorMP <aitor.martinez.pons@gmail.com>
2022-03-26 20:55:32 +0000

    added handlebars formating, nav bar

--- app.js
+++ app.js
@@ -1,23 +1,32 @@
 const express = require('express');
+const hbs = require('hbs');
+
+hbs.registerPartials(__dirname + '/views/partials');
 
 const app = express();
 
+app.set('view engine', 'hbs');
+
+app.set('views', __dirname + '/views');
+
+app.use(express.static('public'));
+
+app.locals.pageTitle = 'Jerry the Storm Trooper';
+
 app.get('/home', (request, response) => {
-  response.sendFile(__dirname + '/views/home.html');
+  response.render('home', { message: 'Jerry' });
 });
 
 app.get('/about', (request, response) => {
-  response.sendFile(__dirname + '/views/about.html');
+  response.render('about', { message: 'About' });
 });
 
 app.get('/works', (request, response) => {
-  response.sendFile(__dirname + '/views/works.html');
+  response.render('works', { message: 'Works' });
 });
 
 app.get('/gallery', (request, response) => {
-  response.sendFile(__dirname + '/views/gallery.html');
+  response.render('gallery', { message: 'Gallery' });
 });
-
-app.use(express.static('public'));
 
 app.listen(3000);
